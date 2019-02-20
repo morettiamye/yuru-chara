52387aee74c4b5628079a764d39fa3e0c8faf4a1
Amy <amyemoretti@gmail.com>
2019-02-20 16:26:55 -0600

    Updating for js

---
+++ mascots.js
@@ -0,0 +1,366 @@
+const mascots = 
+[
+  
+        {
+            "name": "Marimokkori",
+            "japanese": "まりもっこり",
+            "mascot": "Hokkaido Prefecture",
+            "prefecture": "Hokkaido",
+            "description": "The name 'Marimokkori' is a portmanteau: marimo is the word for the green algae clusters that grow in some of Hokkaidō's lakes, while mokkori, literally 'bulge', is a Japanese slang term for an erection.",
+            "picture": "https://japantoday-asset.scdn3.secure.raxcdn.com/img/store/45/be/bd8c258447018ac32111f7eb8eba67ce5b3e/mascots/_w1700.jpg",
+            "official site": "https://ameblo.jp/marimokkori2005/"
+        },
+        {
+            "name": "Melon Bear",
+            "japanese": "メロン熊",
+            "macot": "Yubari-cho (Unofficial)",
+            "prefecture": "Hokkaido",
+            "city": "Yubari",
+            "description": "A bear with a yubari melon on its head.",
+            "picture": "https://mondomascots.com/wp-content/uploads/2016/12/IMG_2215-2.jpg",
+            "official site": "https://ameblo.jp/melon-kuma/"
+        },
+        {
+            "name": "Kyun-chan",
+            "japanese": "キュンちゃん",
+            "mascot": "Hokkaido Tourism",
+            "prefecture": "Hokkaido",
+            "description": "Kyun-chan is a northern pika, a small furry rabbit-like animal (ezo naki usagi) found only in Hokkaido. He's really shy and that's why wears a Hokkaido deer hat (ezo shika) to hide his shyness.",
+            "picture": "https://mondomascots.com/wp-content/uploads/2018/10/image2-1.jpeg",
+            "official site": "http://kyuns-room.visit-hokkaido2.jp/"
+        },
+        {
+            "name": "IburiOne Jr. ",
+            "japanese": "いぶりONE-Jr.",
+            "mascot": "Iburishichou City",
+            "prefecture": "Hokkaido",
+            "city": "Iburishichou",
+            "description": "A pink dog",
+            "picture": "https://mondomascots.com/wp-content/uploads/2018/10/image2-1.jpeg",
+            "official site": "https://www.facebook.com/iburione.jr/"
+
+        },
+        {
+            "name": "Kiko-chan",
+            "japanese": "キーコ",
+            "mascot": "Kikonai City",
+            "prefecture": "Hokkaido",
+            "city": "Kikonai",
+            "description": "A red bull",
+            "picture": "https://mondomascots.com/wp-content/uploads/2018/10/image5-1-e1540564740651.jpeg",
+            "official site": "http://www.uu-hokkaido.jp/corporate/tourism/kikonai/kikonai.shtml"
+        },
+        {
+            "name": "Kazumo-chan",
+            "japanese": "kazumoちゃん",
+            "mascot": "Rumoi City",
+            "prefecture": "Hokkaido",
+            "city": "Rumoi",
+            "description": "A piece of herring roe",
+            "picture": "https://mondomascots.com/wp-content/uploads/2018/10/image1-2.jpeg",
+            "official site": "http://rumoi-rasisa.jp/rumoide/rumoimeguri/kazumo%E3%81%A1%E3%82%83%E3%82%93/"
+        },
+        {
+            "name": "Ikaaru Seijin",
+            "japanese": "イカール星人",
+            "mascot": "Hakodate City",
+            "prefecture": "Hokkaido",
+            "city": "Hakodate",
+            "description": "The Man from Planet Ikaaru, an alien squid",
+            "picture": "https://mondomascots.com/wp-content/uploads/2018/01/image1-1.jpeg",
+            "official site": "http://ika-r.shop-pro.jp/"
+        },
+        {
+            "name": "Jingis Khan No Jin-Kun",
+            "japanese": "ジンギスカンのジンくん",
+            "mascot": "Local delicacy Jingis Khan",
+            "prefecture": "Hokkaido",
+            "description": "Jingis Khan represents a local delicacy by the same name, which is a cook-it-yourself dish including mutton",
+            "picture": "https://mondomascots.com/wp-content/uploads/2017/05/IMG_4108.jpg",
+            "official site": "http://jingisukan-jin.com/"
+        },
+        {
+            "name": "Nipo-kun",
+            "mascot": "Abashiri Prison",
+            "prefecture": "Hokkaido",
+            "city": "Abashiri",
+            "description": "A mascot modeled on a traditional toy made by the local Ainu tribes.",
+            "picture": "https://mondomascots.com/wp-content/uploads/2017/05/%E3%81%AB%E3%81%BB%E3%82%9A%E3%81%8F%E3%82%93%EF%BC%92.jpeg"
+        },
+        {
+            "name": "Sarupu",
+            "japanese": "さるっぷ",
+            "mascot": "Sarufutsu Village",
+            "prefecture": "Hokkaido",
+            "city": "Sarufutsu",
+            "description": "A monkey with a scallop crown and a cow-print onesie. He weighs the same as 90 scallops.",
+            "picture": "https://mondomascots.com/wp-content/uploads/2016/11/IMG_2184-e1479906815294-768x1024-1.jpg",
+            "official site": "http://sarufutsu.jp/character.html"
+        },
+        {
+            "name": "Laveton",
+            "japanese": "らべとん",
+            "mascot": "Kamifurano City",
+            "prefecture": "Hokkaido",
+            "city": "Kamifurano",
+            "description": "A pig dressed in lavendar.  Kamifurano is famous for both lavendar and pork.",
+            "photo": "https://pbs.twimg.com/media/DxRqdjbUwAA2D0k.jpg",
+            "official site": "http://www.kamifurano.jp/yuruchara"
+        },
+        {
+            "name": "Soraisu",
+            "mascot": "Tokaichi Festival",
+            "prefecture": "Hokkaido",
+            "description": "A sky blue squirrel with cloud hair.",
+            "photo": "https://pbs.twimg.com/media/DxcFk-UVAAAB-Bv.jpg"
+        },
+        {
+            "name": "Milkuman",
+            "prefecture": "Hokkaido",
+            "description": "A bear with an ice cream cone on his head."
+        },
+        {
+            "name": "Frep the Fox",
+            "japanese": "フレップ・ザ・フォックス",
+            "mascot": "The Hokkaido Nippon-Ham Fighters Baseball Team",
+            "prefecture": "Hokkaido",
+            "city": "Sapporo",
+            "description": "An Ezo red fox, who debuted in March 2016. He is gray with red markings, and he is considered an 'apprentice.'"
+
+        },
+        {
+            "name": "Polly Polaris",
+            "japanese": "ポリーポラリス",
+            "mascot": "The Hokkaido Nippon-Ham Fighters Baseball Team",
+            "prefecture": "Hokkaido",
+            "city": "Sapporo",
+            "description": "A brown squirrel who appeared late 2012, at the same time when the Fighters announced their 10th season. She's the first female mascot that was introduced to the team."
+        },
+        {
+            "name": "Cubby the Bear",
+            "mascot": "The Hokkaido Nippon-Ham Fighters Baseball Team",
+            "prefecture": "Hokkaido",
+            "city": "Sapporo",
+            "description": "a brown bear and younger brother of B·B. He appeared in 2006 as the mascot of the Fighters' minor league team, based in Kamagaya, Chiba. Cubby and B·B performed for each team, but also sometimes perform together when the Fighters' professional team plays in Tokyo."
+        },
+        {
+            "name": "B.B. (Brisky the Bear)",
+            "mascot": "The Hokkaido Nippon-Ham Fighters Baseball Team",
+            "prefecture": "Hokkaido",
+            "city": "Sapporo",
+            "description": "A black bear with a ruffle of black or orange hair on his head. He first appeared in 2004, as the successor to Fighty. When B·B was first introduced, his appearance was criticized by some of the Fighters' supporters for being too Americanized. On the Fighters official website, B·B has his own photo gallery and column."
+        },
+        {
+            "name": "Fighty",
+            "mascot": "The Hokkaido Nippon-Ham Fighters Baseball Team",
+            "prefecture": "Hokkaido",
+            "city": "Sapporo",
+            "description": "A bright pink pterodactyl whose head resembled a giant leg of ham and who sometimes rode a bicycle around the field. Fighty was retired, despite the (unsuccessful) 'Save Fighty' campaign, when the Fighters moved to Hokkaido.",
+            "status": "Retired"
+        },
+        {
+            "name": "Twakkie or TV Daddy",
+            "mascot": "Sapporo TV Tower",
+            "prefecture": "Hokkaido",
+            "city": "Sapporo",
+            "description": "The Sapporo TV Tower, with a mustache",
+            "photo": "https://pbs.twimg.com/media/DSTYfjlW0AQ8LwO.jpg"
+        },
+        {
+            "name": "Moko",
+            "mascot": "The Japanese Self Defense Force - Hokkaido",
+            "prefecture": "Hokkaido",
+            "city": "Sapporo",
+            "description": "A sheep with a cute camouflage bowtie.",
+            "photo": "https://pbs.twimg.com/media/DmN_tZ9UwAA-kKl.jpg"
+
+        },
+        {
+            "name": "Kibicchi",
+            "mascot": "Odori Park",
+            "prefecture": "Hokkaido",
+            "city": "Sapporo",
+            "description": "A walking ear of corn that represents Odori Park, a park full of carts of fresh corn",
+            "photo": "https://pbs.twimg.com/media/DdRJubbUQAAtf0Q.jpg"
+        },
+        {
+          "name": "Kiyotchi",
+          "mascot": "Kiyota Ward",
+          "prefecture": "Hokkaido",
+          "city": "Sapporo",
+          "description": "His abstract art hat is the local logo, a K for Kiyota.",
+          "photo": "https://pbs.twimg.com/media/DsDv_y8U8AIf6lv.jpg"  
+        },
+        {
+            "name": "Kappon",
+            "mascot": "Jozankei Onsen",
+            "prefecture": "Hokkaido",
+            "city": "Sapporo",
+            "description": "His full name is “Jozankei Kappa Kokichibei”. One day, he was playing with his friends on the banks of the Toyohira River with a bucket that had floated downstream, and shouting out “kappon!” he squeezed himself into the bucket and couldn’t get back out. From that day, everyone called him ‘Kappon’! Kappon loves hot springs and always brings his own towel and bucket with him, because he thinks it’s convenient to bring them to the hot springs himself.",
+            "photo": "https://jozankei.jp/wp-content/uploads/2015/12/photo6.jpg",
+            "official site": "https://jozankei.jp/en/kappon"
+        },
+        {
+            "name": "Ezomon",
+            "mascot": "2017 Sapporo Asian Winter Games",
+            "prefecture": "Hokkaido",
+            "city": "Sapporo",
+            "description": "A Hokkaido flying squirrel",
+            "photo": "https://www.seiko.co.jp/en/branding/report/images/07_ph01.jpg"
+        },
+        {
+            "name": "Dole-kun",
+            "mascot": "Hokkaido Consodole Football Club",
+            "prefecture": "Hokkaido",
+            "city": "Sapporo",
+            "description": "Modeled on the Blakiston's Fish Owl -- the largest owl in Japan and a very well-known denizen of Hokkaido.",
+            "picture": "http://www.sapporo.travel/cms/wp-content/uploads/2016/05/consa_sub_01-360x270.jpg"
+        },
+        {
+            "name": "Momiji-chan",
+            "japanese": "はしあさこ",
+            "prefecture": "Hokkaido",
+            "description": "A gun toting pink yezo sika deer",
+            "picture": "https://66.media.tumblr.com/6dd5fa58a86c9cf7ed58ada61c07235c/tumblr_oh0iuu5DX41un0a58o1_r1_1280.jpg"
+        },
+
+
+        {
+            "name:": "Ikube",
+            "mascot": "Aomori Prefecture",
+            "prefecture": "Aomori",
+            "description": "He is a blue traveling fairy who fell in love with Aomori prefecture after wandering around Japan. Because of this love, he decided to live in Aomori (which means 'blue forest')"
+        },
+        {
+            "name": "Wanko",
+            "mascot": "Iwate Prefecture",
+            "prefecture": "Iwate", 
+            "description": "Inspired by a 'wanko soba' noodle, a famous local food in Iwate prefecture. Wanko consists of 5 brothers, each of them has a different motif and character."
+        },
+        {
+            "name": "Musubimaru", 
+            "mascot": "Miyagi Prefecture",
+            "prefecture": "Miyagi",
+            "description": "A rice ball samurai. Rice is famous produce in Miyagi and its armor design comes after the local hero'Masamune Date'"
+        },
+        {
+            "name": "Sugitchi",
+            "mascot": "Sugitchi",
+            "prefecture": "Akita",
+            "description": "An Akita cedar tree, retired in 2017 after 10 years of service"
+        },
+        {
+            "name": "Kizakura Sui",
+            "japanese": "黄桜すいプロジェクト",
+            "mascot": "Akita Prefecture",
+            "prefecture": "Akita",
+            "description": "A 16-year-old water goddess that is 158 centimeters tall and likes the Akita Komachi variety of rice, soft serve ice cream, and cats",
+            "official site": "https://kizakurasui.jp/"
+        },
+        {
+            "name": "Kitekeru-kun",
+            "mascot": "Yamagata Prefecture",
+            "prefecture": "Yamagata",
+            "description": "This mascot is overshadowed by Kumamons popularity."
+        },
+        {
+            "name" : "Kibitan",
+            "mascot": "Fukushima Prefecture",
+            "prefecture": "Fukushima",
+            "description": "Based on the local 'Kibitaki' bird (the narcissus flycatcher). He was originally created for a nineties athletics event and now represents hopeful renewal for Fukushima."
+        },
+        {
+            "name": "Hustle Komon",
+            "mascot": "Ibaraki Prefecture",
+            "prefecture": "Ibaraki",
+            "description": "A character inspired by the long-running TV period drama, Mito Komon."
+        },
+        {
+            "name": "Tochimarukun",
+            "japanese": "とちまるくん",
+            "mascot": "Tochigi Prefecture",
+            "prefecture": "Tochigi",
+            "description": "A little hard worker",
+            "official site": "http://tochimarukun.jp/"
+
+        },
+        {
+            "name": "Gunma-chan",
+            "japanese": "ぐんまちゃんの",
+            "mascot": "Gunma Prefecture",
+            "prefecture": "Gunma",
+            "description": "A cute pony from Gunma prefecture",
+            "official site": "http://www.gunmachan-navi.pref.gunma.jp/"
+        },
+        {
+            "name": "Kobaton",
+            "japanese": "コバトン",
+            "mascot": "Saitama Prefecture",
+            "prefecture": "Saitama",
+            "description": "The mascot of Saitama prefecture, Kobaton is a Eurasian collared dove, which is also the prefectural bird. Kobaton was made originally as the mascot of the fifty-ninth annual national athletic meeting held in the prefecture in 2004, and was inaugurated as mascot of the prefecture in 2005 with an inauguration ceremony and a letter of appointment from the governor. A wheelchair-using version of Kobaton also exists.", 
+            "official site": "http://www.kobatonya.com/"
+        },
+        {
+            "name": "Chiba-kun",
+            "mascot": "Chiba Prefecture",
+            "description": "A mysterious red animal from Chiba Prefecture whose shape comes from the geography of the prefecture"
+        },
+        {
+            "name": "Reruhi-San",
+            "japanese": "レルヒさん",
+            "mascot": "Niigata Prefecture",
+            "prefecture": "Niigata",
+            "description": "TThe 270cm-tall skiing mascot based on Theodor Von-Lerch, an Austro-Hungarian major who brought the skiing to Japan"
+        },
+        {
+            "name": "Kitokito-kun",
+            "mascot": "Toyama Prefecture",
+            "prefecture": "Toyama"
+        },
+        {
+            "name": "Hyakumansan",
+            "japanese": "百万さん",
+            "mascot": "Ishikawa Prefecture",
+            "prefecture": "Ishikawa",
+            "description": "'Mr. One Million', generally unpopular because he is considered to be ugly"
+        },
+        {
+            "name": "Sautan",
+            "japanese": "サウタン",
+            "mascot": "Fukui Prefecture",
+            "prefecture": "Fukui",
+            "description": "A dinosaur, meant to represent the many dinosaur fossils found in Fukui"
+        },
+        {
+            "name": "Hishimaru Takeda",
+            "japanese": "菱丸",
+            "mascot": "Yamanashi Prefecture",
+            "prefecture": "Yamanashi",
+            "description": "A member of the Takeda clan",
+            "official site": "https://www.facebook.com/takeda.hishimaru/"
+        },
+        {
+            "name": "Arukuma",
+            "mascot": "Nagano Prefecture",
+            "prefecture": "Nagano",
+            "description": "A walking bear with an Nagano apple hat"
+        },
+        {
+            "name": "Minamo",
+            "japanese": "ミナモ",
+            "mascot": "Gifu Prefecture",
+            "prefecture": "Gifu",
+            "description": "A smiling sprite"
+        },
+        {
+            "name": "Fujippi",
+            "japanese": "ふじっぴー",
+            "mascot": "Shizuoka Prefecture",
+            "prefecture": "Shizuoka",
+            "description": "If Mt Fuji was a person",
+            "official site": "http://www.pref.shizuoka.jp/kikaku/ki-110/fujippi/index.html"
+        },
+    ]
+
+
+export default mascots;
